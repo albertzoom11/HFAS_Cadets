16f38e8af4ac374c613ca5a9cb44fd5549878af4
albertzoom11 <52132982+albertzoom11@users.noreply.github.com>
2020-06-21 19:27:51 -0400

    login page ui

--- hfas_cadets/lib/screens/authentication/sign_in.dart
+++ hfas_cadets/lib/screens/authentication/sign_in.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hfascadets/screens/services/auth.dart';
+import 'package:hfascadets/buttons/googleSignIn.dart';
+import 'package:hfascadets/animation/fadeAnimation.dart';
 
 class SignIn extends StatefulWidget {
   final Function toggleView;
@@ -21,83 +23,241 @@ class _SignInState extends State<SignIn> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue[50],
-      appBar: AppBar(
-        backgroundColor: Colors.blue[900],
-        elevation: 0.0,
-        title: Text('Sign in'),
-        actions: <Widget>[
-          FlatButton.icon(
-            icon: Icon(Icons.person),
-            label: Text('Sign up'),
-            onPressed: () {
-              widget.toggleView();
-            },
-          ),
-        ],
-      ),
       body: Container(
-        padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              SizedBox(height: 20.0),
-              TextFormField(
-                decoration: InputDecoration(
-                  icon: Icon(Icons.email),
-                  hintText: 'Email',
+        width: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(begin: Alignment.topCenter, colors: [
+            Colors.indigo[900],
+            Colors.indigo[800],
+            Colors.blue[500],
+          ]),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    SizedBox(
+                      height: 80,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(20),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          FadeAnimation(
+                              1.1,
+                              Text(
+                                'Login',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 40,
+                                ),
+                              )),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          FadeAnimation(
+                              1.2,
+                              Text(
+                                'Welcome Back',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 18,
+                                ),
+                              )),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
-                validator: (val) => val.isEmpty ? 'Please enter an email or sign in with Google.' : null,
-                onChanged: (val) {
-                  setState(() {
-                    email = val;
-                  });
-                },
+                SizedBox(width: 120,),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(0, 0, 20.0, 0),
+                  child: FadeAnimation(1.3, Image(
+                    image: AssetImage('assets/images/hfasLogo.png'), height: 150,
+                  )),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(60),
+                      topRight: Radius.circular(60)),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.all(30),
+                  child: Column(
+                    children: <Widget>[
+                      SizedBox(
+                        height: 60,
+                      ),
+                      FadeAnimation(
+                          1.4,
+                          Container(
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(10),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Color.fromRGBO(225, 95, 27, .3),
+                                    blurRadius: 20,
+                                    offset: Offset(0, 10),
+                                  )
+                                ]),
+                            child: Column(
+                              children: <Widget>[
+                                Container(
+                                  padding: EdgeInsets.all(10),
+                                  decoration: BoxDecoration(
+                                    border: Border(
+                                        bottom: BorderSide(
+                                            color: Colors.grey[200])),
+                                  ),
+                                  child: TextField(
+                                    decoration: InputDecoration(
+                                      hintText: 'Email',
+                                      hintStyle: TextStyle(color: Colors.grey),
+                                      border: InputBorder.none,
+                                    ),
+                                  ),
+                                ),
+                                Container(
+                                  padding: EdgeInsets.all(10),
+                                  decoration: BoxDecoration(
+                                    border: Border(
+                                        bottom: BorderSide(
+                                            color: Colors.grey[200])),
+                                  ),
+                                  child: TextField(
+                                    decoration: InputDecoration(
+                                      hintText: 'Password',
+                                      hintStyle: TextStyle(color: Colors.grey),
+                                      border: InputBorder.none,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          )),
+                      SizedBox(
+                        height: 40,
+                      ),
+                      FadeAnimation(
+                          1.5,
+                          Text(
+                            'Forgot Password?',
+                            style: TextStyle(color: Colors.grey),
+                          )),
+                      SizedBox(
+                        height: 40,
+                      ),
+                      FadeAnimation(
+                          1.6,
+                          Container(
+                            height: 50,
+                            margin: EdgeInsets.symmetric(horizontal: 50),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(50),
+                              color: Colors.indigo[900],
+                            ),
+                            child: Center(
+                              child: Text(
+                                'Log in',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          )),
+                      SizedBox(
+                        height: 50,
+                      ),
+                      FadeAnimation(
+                          1.7,
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: <Widget>[
+                              Expanded(
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: <Widget>[
+                                    Divider(
+                                      color: Colors.grey,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 10.0),
+                                child: Text(
+                                  'OR',
+                                  style: TextStyle(
+                                      color: Colors.grey,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                              Expanded(
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: <Widget>[
+                                    Divider(
+                                      color: Colors.grey,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          )),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Row(
+                        children: <Widget>[
+                          Expanded(
+                            child: FadeAnimation(1.8, googleSignInButton()),
+                          ),
+                          SizedBox(
+                            width: 20,
+                          ),
+                          Expanded(
+                            child: FadeAnimation(
+                                1.9,
+                                Container(
+                                  height: 50,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(50),
+                                    color: Colors.blue[900],
+                                  ),
+                                  child: Center(
+                                    child: Text(
+                                      'Sign Up',
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  ),
+                                )),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
               ),
-              SizedBox(height: 20.0),
-              TextFormField(
-                decoration: InputDecoration(
-                  icon: Icon(Icons.lock),
-                  hintText: 'Password',
-                ),
-                obscureText: true,
-                validator: (val) => val.length < 6 ? 'Please enter a password with at least 6 characters.' : null,
-                onChanged: (val) {
-                  setState(() {
-                    password = val;
-                  });
-                },
-              ),
-              SizedBox(height: 20.0),
-              RaisedButton(
-                color: Colors.blue[900],
-                child: Text(
-                  'Sign In',
-                  style: TextStyle(color: Colors.white),
-                ),
-                onPressed: () async {
-                  if (_formKey.currentState.validate()) {
-                    dynamic result = await _auth.signInWithEmailAndPassword(email, password);
-                    if (result == null) {
-                      setState(() {
-                        error = 'Your email or password was incorrect.\nPlease try again.';
-                      });
-                    }
-                  }
-                },
-              ),
-              SizedBox(height: 12.0),
-              Text(
-                error,
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Colors.red,
-                  fontSize: 14.0,
-                ),
-              )
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
