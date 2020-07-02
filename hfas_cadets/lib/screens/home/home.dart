5d84a76942be87128b67e1776f2499a7844663bf
albertzoom11 <52132982+albertzoom11@users.noreply.github.com>
2020-07-01 23:17:35 -0400

    bottom nav bar finished with elevation

--- hfas_cadets/lib/screens/home/home.dart
+++ hfas_cadets/lib/screens/home/home.dart
@@ -1,52 +1,79 @@
-import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:hfascadets/screens/services/auth.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
   final AuthService _auth = AuthService();
+  int _selectedItemIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue[50],
-      appBar: AppBar(
-        title: Text('HFAS Cadets'),
-        centerTitle: true,
-        backgroundColor: Colors.blue[900],
-        elevation: 0.0,
-        actions: <Widget>[
-          FlatButton.icon(
-            icon: Icon(Icons.person),
-            onPressed: () async {
-              dynamic result = await _auth.signOutGoogle();
-              if (result == null) {
-                print('sign out failed');
-              } else {
-                Navigator.pushNamedAndRemoveUntil(context, '/mainMenu', (route) => false);
-              }
-            },
-            label: Text('Sign out'),
-          ),
-        ],
-      ),
-      bottomNavigationBar: CurvedNavigationBar(
-        color: Colors.blue[900],
         backgroundColor: Colors.blue[50],
-        buttonBackgroundColor: Colors.blue[800],
-        height: 50,
-        items: <Widget>[
-          Icon(Icons.home, size: 25, color: Colors.black,),
-          Icon(Icons.chat_bubble, size: 25, color: Colors.black,),
-          Icon(Icons.add_circle, size: 25, color: Colors.black,),
-          Icon(Icons.view_list, size: 25, color: Colors.black,),
-          Icon(Icons.person, size: 25, color: Colors.black,),
-        ],
-        index: 0,
-        animationDuration: Duration(milliseconds: 200),
-        animationCurve: Curves.bounceInOut,
-        onTap: (index) {
-          print('Current Index is $index');
-        },
+        appBar: AppBar(
+          title: Text('HFAS Cadets'),
+          centerTitle: true,
+          backgroundColor: Colors.blue[900],
+          elevation: 0.0,
+          actions: <Widget>[
+            FlatButton.icon(
+              icon: Icon(Icons.person),
+              onPressed: () async {
+                dynamic result = await _auth.signOutGoogle();
+                if (result == null) {
+                  print('sign out failed');
+                } else {
+                  Navigator.pushNamedAndRemoveUntil(context, '/mainMenu', (route) => false);
+                }
+              },
+              label: Text('Sign out'),
+            ),
+          ],
+        ),
+        bottomNavigationBar: Material(
+          elevation: 40,
+          child: Container(
+            child: Row(
+              children: <Widget>[
+                buildNavBarItem(Icons.home, 0),
+                buildNavBarItem(Icons.date_range, 1),
+                buildNavBarItem(Icons.add_circle, 2),
+                buildNavBarItem(Icons.view_list, 3),
+                buildNavBarItem(Icons.person, 4),
+              ],
+            ),
+          ),
+        )
+    );
+  }
+
+  Widget buildNavBarItem(IconData icon, int index) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          _selectedItemIndex = index;
+        });
+      },
+      child: Container(
+        height: 60,
+        width: MediaQuery.of(context).size.width/5,
+        decoration: index == _selectedItemIndex ? BoxDecoration(
+          border: Border(
+            bottom: BorderSide(width: 4, color: Colors.blue[900])
+          ),
+          gradient: LinearGradient(colors: [
+            Colors.blue.withOpacity(0.3),
+            Colors.blue.withOpacity(0.015),
+          ],
+          begin: Alignment.bottomCenter, end: Alignment.topCenter,
+          )
+//          color: index == _selectedItemIndex ? Colors.blue[900] : Colors.white,
+        ) : BoxDecoration(),
+        child: Icon(icon, color: index == _selectedItemIndex ? Colors.blue[900] : Colors.grey,),
       ),
     );
   }
